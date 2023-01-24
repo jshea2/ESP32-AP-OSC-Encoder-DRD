b8927f4c802c95b3e7b6ba68cbe547749ae62235
Joe Shea <70780576+jshea2@users.noreply.github.com>
2023-01-23 19:04:52 -0800

    modified:   platformio.ini 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,23 @@
 #include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
 #include <ArduinoOSCWiFi.h>
-#include <ezButton.h>
 #include <ArduinoJson.h>  //Warning - only versions prior 6 are compatible (5.11)
 #include <FS.h>
 #include <SPIFFS.h>
 #include <EasyLed.h>
+#include <ESP32Encoder.h>
 
 WiFiManager wm;
 WiFiManagerParameter custom_osc_ip("oscip", "OSC IP", "", 16);
-WiFiManagerParameter custom_osc_port("oscport", "OSC Port", "53000", 8);
-WiFiManagerParameter custom_osc_address("oscaddress", "OSC Address", "/go", 8);
-WiFiManagerParameter custom_osc_argument("oscargument", "OSC Argument", "", 8);
-WiFiManagerParameter custom_button_pin("buttonpin", "Button Pin (GPIO)", "13", 2);
+WiFiManagerParameter custom_osc_port("oscport", "OSC Port", "8000", 8);
+WiFiManagerParameter custom_osc_address("oscaddress", "OSC Address", "/eos/user=7/chan/300", 40);
+WiFiManagerParameter custom_osc_argument("oscargument", "OSC Argument", "", 16);
+// WiFiManagerParameter custom_button_pin("buttonpin", "Button Pin (GPIO)", "13", 2);
+WiFiManagerParameter custom_encoder_clk("encoderclkpin", "Encoder CLK Pin", "33", 8);
+WiFiManagerParameter custom_encoder_dt("encoderdtpin", "Encoder DT Pin", "15", 8);
+
+//----------------------------------------------------------------
+//Setup Encoder
+ESP32Encoder encoder;
 
 //----------------------------------------------------------------
 //Double Reset Detector
@@ -36,23 +42,11 @@ EasyLed led(2, EasyLed::ActiveLevel::High);
 void doubleResetConnector () {  
   drd = new DoubleResetDetector(DRD_TIMEOUT, DRD_ADDRESS);
   if (drd->detectDoubleReset()) { Serial.println(F("DRD")); initialConfig = true; }
-  // ESP_WiFiManager ESP_wifiManager("ConfigOnDoubleReset");
-  // if (ESP_wifiManager.WiFi_SSID() == "") { Serial.println(F("No AP credentials")); initialConfig = true; }
   if (initialConfig) {
     //reset settings - wipe credentials for testing
     wm.resetSettings();
     Serial.println(F("Starting Config Portal")); led.flash();
-    //if (!ESP_wifiManager.startConfigPortal()) { Serial.println(F("Not connected to WiFi")); }
-    //else { Serial.println(F("connected")); }
   }
-  // else { WiFi.mode(WIFI_STA); WiFi.begin(); } 
-  // unsigned long startedAt = millis();
-  // digitalWrite(PIN_LED, LOW); Serial.print(F("After waiting "));
-  // int connRes = WiFi.waitForConnectResult();
-  // float waited = (millis() - startedAt);
-  // Serial.print(waited / 1000); Serial.print(F(" secs , Connection result is ")); Serial.println(connRes);
-  // if (WiFi.status() != WL_CONNECTED) { Serial.println(F("Failed to connect")); }
-  // else { Serial.print(F("Local IP: ")); Serial.println(WiFi.localIP()); }
 }
 
 //----------------------------------------------------------------
@@ -67,7 +61,9 @@ void saveCustomConfig () {
     int send_port = std::stoi(custom_osc_port.getValue());
     const char *oscaddr = custom_osc_address.getValue();
     const char *oscarg = custom_osc_argument.getValue();
-    int buttonpin = std::stoi(custom_button_pin.getValue());
+    int encoderclkpin = std::stoi(custom_encoder_clk.getValue());
+    int encoderdtpin = std::stoi(custom_encoder_dt.getValue());
+    // int buttonpin = std::stoi(custom_button_pin.getValue());
 
     Serial.println("saving config");
     led.on();
@@ -77,7 +73,9 @@ void saveCustomConfig () {
     json["send_port"] = send_port;
     json["oscaddr"] = oscaddr;
     json["oscarg"] = oscarg;
-    json["buttonpin"] = buttonpin;
+    json["clkpin"] = encoderclkpin;
+    json["dtpin"] = encoderdtpin;
+    // json["buttonpin"] = buttonpin;
 
     File configFile = SPIFFS.open("/config.json", "w");
     if (!configFile) {
@@ -110,15 +108,22 @@ void saveParamsCallback () {
   Serial.print(" : ");
   Serial.println(custom_osc_argument.getValue());
 
-  Serial.print(custom_button_pin.getID());
+  Serial.print(custom_encoder_clk.getID());
   Serial.print(" : ");
-  Serial.println(custom_button_pin.getValue());
+  Serial.println(custom_encoder_clk.getValue());
+
+  Serial.print(custom_encoder_dt.getID());
+  Serial.print(" : ");
+  Serial.println(custom_encoder_dt.getValue());
+
 }
 
 //----------------------------------------------------------------
 //Declare and link params to consts
-int buttonpin = std::stoi(custom_button_pin.getValue());
-ezButton button(buttonpin);
+// int buttonpin = std::stoi(custom_button_pin.getValue());
+
+int encoderclkpin = std::stoi(custom_encoder_clk.getValue());
+int encoderdtpin = std::stoi(custom_encoder_dt.getValue());
 
 const char *host = custom_osc_ip.getValue();
 int send_port = std::stoi(custom_osc_port.getValue());
@@ -130,10 +135,11 @@ const char *oscarg = custom_osc_argument.getValue();
 void setup() {
     WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP    
     Serial.begin(115200);
-    button.setDebounceTime(50);
     doubleResetConnector();
     led.flash();
     wm.setDarkMode(true);
+    encoder.attachHalfQuad ( encoderdtpin, encoderclkpin );
+    encoder.setCount ( 0 );
 
     
 
@@ -145,7 +151,10 @@ void setup() {
     wm.addParameter(&custom_osc_port);
     wm.addParameter(&custom_osc_address);
     wm.addParameter(&custom_osc_argument);
-    wm.addParameter(&custom_button_pin);
+    wm.addParameter(&custom_encoder_clk);
+    wm.addParameter(&custom_encoder_dt);
+
+    // wm.addParameter(&custom_button_pin);
     wm.setShowStaticFields(true);
     wm.setConfigPortalBlocking(true);
 
@@ -179,7 +188,8 @@ void setup() {
           strcpy(const_cast<char*>(std::to_string(send_port).c_str()), json["send_port"]);
           strcpy(const_cast<char *>(oscaddr), json["oscaddr"]);
           strcpy(const_cast<char *>(oscarg), json["oscarg"]);
-          strcpy(const_cast<char*>(std::to_string(buttonpin).c_str()), json["buttonpin"]);
+          strcpy(const_cast<char*>(std::to_string(encoderclkpin).c_str()), json["clkpin"]);
+          strcpy(const_cast<char*>(std::to_string(encoderdtpin).c_str()), json["dtpin"]);
         } else {
           Serial.println("failed to load json config");
         }
@@ -222,26 +232,47 @@ void loop() {
     drd->loop();
     wm.process();
     OscWiFi.update();
-    button.loop();
+    long newPosition = encoder.getCount() / 2;
+    //Serial.println(newPosition);
+
+    if (newPosition < 0){
+        encoder.clearCount();
+        Serial.println("MINUS ONE");
+        std::string oscaddr_str(oscaddr);
+        oscaddr_str += "/-%";
+        const char* updated_oscaddr = oscaddr_str.c_str();
+        OscWiFi.send(host, send_port, updated_oscaddr, oscarg);
+        Serial.println(updated_oscaddr);
+    }
+
+    if (newPosition > 0){
+      encoder.clearCount();
+      Serial.println("PLUS ONE");
+      std::string oscaddr_str(oscaddr);
+      oscaddr_str += "/+%";
+      const char* updated_oscaddr = oscaddr_str.c_str();
+      OscWiFi.send(host, send_port, updated_oscaddr, oscarg);
+      Serial.println(updated_oscaddr);
+    }
 
     // put your main code here, to run repeatedly:
-    if(button.isPressed())
-  {
-    Serial.println("The button is pressed");
-    Serial.println(host);
-    Serial.println(send_port);
-    Serial.println(oscaddr);
-    Serial.println(oscarg);
-    Serial.println(buttonpin);
-    OscWiFi.send(host, send_port, oscaddr, oscarg);
-    // return;
-  }
-  if (button.isReleased())
-  {
-    Serial.println("The button is released");
-    //OscWiFi.send(host, send_port, "/off");
-    // or do that separately
-    // OscWiFi.parse(); // to receive osc
-    // OscWiFi.post(); // to publish osc
-  }
+  //   if(button.isPressed())
+  // {
+  //   Serial.println("The button is pressed");
+  //   Serial.println(host);
+  //   Serial.println(send_port);
+  //   Serial.println(oscaddr);
+  //   Serial.println(oscarg);
+  //   // Serial.println(buttonpin);
+  //   OscWiFi.send(host, send_port, oscaddr, oscarg);
+  //   // return;
+  // }
+  // if (button.isReleased())
+  // {
+  //   Serial.println("The button is released");
+  //   //OscWiFi.send(host, send_port, "/off");
+  //   // or do that separately
+  //   // OscWiFi.parse(); // to receive osc
+  //   // OscWiFi.post(); // to publish osc
+  // }
 }
